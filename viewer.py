ef376e56932f2a4458a1854e848e1b0d6e20f972
Charlie Bareham <barehamcharlie@gmail.com>
2024-01-22 15:31:46 +0000

    Filter table columns

--- viewer.py
+++ viewer.py
@@ -20,13 +20,35 @@ def create_table(database: Database):
     library_names = [library.name for library in database.get_libraries()]
 
     # create table
-    return sg.Table(
+    table = sg.Table(
         book_data,
         headings=["Title", "Read"] + library_names,
+        enable_click_events=True,
     )
+
+    return table
+
+
+def filter_table(table: sg.Table, col: int):
+    # prompt user for desired value
+    column_name = table.ColumnHeadings[col]
+    value = sg.popup_get_text(f"Filter {column_name}: ")
+
+    # update table
+    table_data = table.Values
+    new_table_data = []
+    for row in table_data:
+        if row[col] == value:
+            new_table_data.append(row)
+
+    table.update(values=new_table_data)
 
 
 def main():
+    # TODO: add filtering columns
+    # TODO: add searching for books
+    # TODO: add tags to books
+
     sg.theme("DarkTeal2")
 
     database = Database("database.db")
@@ -36,13 +58,20 @@ def main():
     layout = [
         [table],
     ]
-    window = sg.Window("Books", layout)
+    window = sg.Window(
+        "Books",
+        layout,
+    )
 
     # main loop
     while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED or event == "Cancel":
             break
+        elif event[:2] == (0, "+CLICKED+"):
+            _, _, (row, col) = event
+            if row == -1:
+                filter_table(table, col)
 
     window.close()
 
