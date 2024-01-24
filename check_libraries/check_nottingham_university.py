864121e763ee57685af2f107d58004a2667d08fe
Charlie Bareham <barehamcharlie@gmail.com>
2024-01-24 16:27:15 +0000

    Make script check all books

--- check_libraries/check_nottingham_university.py
+++ check_libraries/check_nottingham_university.py
@@ -1,23 +1,31 @@
+import argparse
 import os
 from configparser import ConfigParser
-from typing import Optional
+from multiprocessing.pool import ThreadPool
+from threading import current_thread
+from typing import Optional, Dict
 from urllib.parse import urlencode, quote
 
-import aiohttp
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver import FirefoxProfile
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.firefox.options import Options
 from yarl import URL
 
 from check_libraries.common import check_titles
-from database import Book
+from database import Book, Database, LibrarySystem
+
+library = LibrarySystem("Nottingham University")
+
+# One webdriver for each thread. Maps thread name to driver.
+webdrivers: Dict[str, WebDriver] = {}
 
 
-def get_book_nottingham_university(
+def get_book(
     book: Book,
+    driver: WebDriver,
 ) -> Optional[URL]:
     """Search Nottingham University library for the book
 
@@ -25,18 +33,6 @@ def get_book_nottingham_university(
     search results or the book is returned. Else,
     None is returned.
     """
-    root = os.path.dirname(__file__)
-    config_path = os.path.join(root, "..", "config.ini")
-
-    parser = ConfigParser()
-    parser.read(config_path)
-
-    # instantiate driver
-    options = Options()
-    options.binary = FirefoxBinary(parser.get("firefox", "executable"))
-    options.profile = FirefoxProfile(parser.get("firefox", "profile"))
-    options.add_argument("--headless")
-    driver = webdriver.Firefox(options=options)
 
     driver.implicitly_wait(10.0)
 
@@ -75,11 +71,87 @@ def get_book_nottingham_university(
     return None
 
 
-def main():
-    book = Book(title="Cannery Row")
-    url = get_book_nottingham_university(book)
+def process_book(
+    book: Book,
+    database_path: str,
+) -> Optional[URL]:
+    driver = webdrivers[current_thread().name]
+    url = get_book(book, driver)
 
-    print(url)
+    database = Database(database_path)
+    database.add_library_book(library, book, url is not None)
+
+    print(book.title, url)
+
+    return url
+
+
+def main():
+    root = os.path.dirname(__file__)
+
+    parser = argparse.ArgumentParser(
+        prog="check_nottingham_libraries",
+        description="Check which books are available in Nottingham libraries.",
+    )
+    parser.add_argument(
+        "-d",
+        "--database",
+        type=str,
+        default=os.path.join(root, "..", "database.db"),
+        help="Path to database containing books to check.",
+    )
+    parser.add_argument(
+        "-n",
+        "--num-workers",
+        type=int,
+        default=10,
+        help="Number of threads to use.",
+    )
+
+    args = parser.parse_args()
+
+    # open config file
+    config_path = os.path.join(root, "..", "config.ini")
+    config_parser = ConfigParser()
+    config_parser.read(config_path)
+
+    # webdriver options
+    options = Options()
+    options.binary = FirefoxBinary(
+        config_parser.get("firefox", "executable")
+    )
+    options.profile = FirefoxProfile(
+        config_parser.get("firefox", "profile")
+    )
+    options.add_argument("--headless")
+
+    # open database
+    database = Database(args.database)
+    database.add_library_system(library)
+
+    # get books from database
+    books = database.get_books()
+
+    # only check books that haven't been checked yet
+    books = [
+        book
+        for book in books
+        if database.check_book_in_library(book, library) is None
+    ]
+
+    # initialise thread pool, and one selenium webdriver for each thread
+
+    def init():
+        driver = webdriver.Firefox(options=options)
+        webdrivers[current_thread().name] = driver
+
+    pool = ThreadPool(args.num_workers, init)
+
+    # process books
+    pool.starmap(
+        process_book,
+        ((book, args.database) for book in books),
+    )
 
 
 if __name__ == "__main__":
