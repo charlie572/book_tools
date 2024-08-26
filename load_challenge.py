016120c9f3020ed9b12bedbcb761cc8684c9257b
Charlie <psycb8@nottingham.ac.uk>
2024-08-26 19:47:55 +0100

    Go through all pages of challenge

--- load_challenge.py
+++ load_challenge.py
@@ -1,9 +1,25 @@
 import argparse
+import urllib
+from urllib.parse import urlparse
 
 import requests
 from bs4 import BeautifulSoup
 
 from database import Database, Book, Challenge, NotFound
+
+
+def iter_challenge_pages(challenge_id):
+    page = 1
+    while True:
+        url = f"https://app.thestorygraph.com/reading_challenges/{challenge_id}"
+        params = {
+            "page": page,
+            "_": 1724694396276,
+        }
+        response = requests.get(url, params=params)
+        soup = BeautifulSoup(response.text, "html.parser")
+        yield soup
+        page += 1
 
 
 def main():
@@ -27,31 +43,38 @@ def main():
 
     database = Database(args.database)
 
-    response = requests.get(args.challenge_url)
-    soup = BeautifulSoup(response.text, "html.parser")
+    url = urlparse(args.challenge_url)
+    challenge_id = url.path.split("/")[-1]
 
-    title = soup.find("h5")
-    challenge_name = title.text
+    for soup in iter_challenge_pages(challenge_id):
+        title = soup.find("h5")
+        challenge_name = title.text
 
-    challenge = Challenge(challenge_name)
-    database.add_challenge(challenge)
+        challenge = Challenge(challenge_name)
+        database.add_challenge(challenge)
 
-    for link in soup.find_all("a"):
-        if not link.attrs["href"].startswith("/books"):
-            continue
+        book_added = False
+        for link in soup.find_all("a"):
+            if not link.attrs["href"].startswith("/books"):
+                continue
 
-        # get title
-        lines = [line for line in link.text.split("\n") if line]
-        book_title = lines[0]
-        book_title = "".join(c for c in book_title if c.isalnum() or c.isspace())  # the titles in the database don't have punctuation
+            # get title
+            lines = [line for line in link.text.split("\n") if line]
+            book_title = lines[0]
+            print(book_title)
 
-        # search for book
-        book = Book(title=book_title)
-        if not database.get_book(book):
-            continue
+            # search for book
+            book = Book(title=book_title)
+            if not database.get_book(book):
+                continue
 
-        # add to challenge
-        database.add_book_to_challenge(book, challenge)
+            # add to challenge
+            database.add_book_to_challenge(book, challenge)
+
+            book_added = True
+
+        if not book_added:
+            break
 
 
 if __name__ == "__main__":
