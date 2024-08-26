0fcc7081d15c7ad6684303c87a0186b9ecfbf951
Charlie <psycb8@nottingham.ac.uk>
2024-08-26 17:13:58 +0100

    set up project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='book_tools',
+    version='',
+    packages=['', 'check_libraries'],
+    url='',
+    license='',
+    author='charlie',
+    author_email='',
+    description=''
+)
